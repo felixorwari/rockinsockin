341d7a21242d746570f6e447be1a99eae863b42a
Felix Orwari <felix.orwari@gmail.com>
2024-05-13 16:53:24 +0300

    Reformatted markup

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,109 +1,111 @@
 app.component('product-display', {
   template:
     /*html*/
-    `<div class="flex flex-col gap-8 px-4 py-8 product-display md:px-0">
-    <div
-      v-for="product in products"
-      :key="product.id"
-      class="flex flex-col product-container md:flex-row md:gap-6"
-    >
-      <!-- Product Image -->
+    `
+    <div class="flex flex-col gap-8 px-4 py-8 product-display md:px-0">
       <div
-        class="w-full overflow-hidden bg-white border-2 border-gray-100 rounded product-image md:w-1/2"
+        v-for="product in products"
+        :key="product.id"
+        class="flex flex-col product-container md:flex-row md:gap-6"
       >
-        <img
-          :src="product.selectedVariant.image"
-          :alt="product.title"
-          class="hover:scale-[110%] object-cover transition"
-        />
-      </div>
-
-      <!-- Product Info -->
-      <div
-        class="w-full product-info md:pt-4 md:pb-8 md:w-1/2 text-balance"
-      >
-        <!-- Inventory Status -->
-        <div class="flex items-center mb-4 text-xs inventory">
-          <span
-            v-if="product.totalInvetory > stockThreshold"
-            class="px-4 py-2 text-green-700 bg-green-100 rounded"
-          >
-          <i class="fa-regular fa-circle-check mr-2 "></i> In stock
-          </span>
-
-          <span
-            v-else-if="product.totalInvetory <= stockThreshold && product.totalInvetory > 0"
-            class="px-4 py-2 text-yellow-700 bg-yellow-100 rounded"
-          >
-          <i class="fa-regular fa-triangle-exclamation mr-2 "></i> Few items remaining!
-          </span>
-
-          <span v-else class="px-4 py-2 text-red-700 bg-red-100 rounded">
-          <i class="fa-regular fa-circle-exclamation mr-2 "></i>  Sold out!
-          </span>
+        <!-- Product Image -->
+        <div
+          class="w-full overflow-hidden bg-white border-2 border-gray-100 rounded product-image md:w-1/2"
+        >
+          <img
+            :src="product.selectedVariant.image"
+            :alt="product.title"
+            class="hover:scale-[110%] object-cover transition"
+          />
         </div>
 
-        <h1 class="product-title">
-          {{ product.brand + ' ' + product.name }}
-        </h1>
-
-        <p class="product-description justify">{{ product.description }}</p>
-
-        <div class="my-4">
-          <p class="font-semibold text-2xl">Ksh. {{ product.price.toLocaleString() }}/-</p>
-        </div>
-
-        <h4>Material</h4>
-        <ul>
-          <li v-for="productDetail in product.details">
-            {{ productDetail }}
-          </li>
-        </ul>
-
-        <h4>Colors</h4>
-        <div class="flex gap-8">
-          <div
-            v-for="variant in product.variants"
-            :key="variant.id"
-            @click="updateVariant(product, variant)"
-            class="flex items-center group hover:cursor-pointer"
-          >
+        <!-- Product Info -->
+        <div
+          class="w-full product-info md:pt-4 md:pb-8 md:w-1/2 text-balance"
+        >
+          <!-- Inventory Status -->
+          <div class="flex items-center mb-4 text-xs inventory">
             <span
-              :style="{ borderColor: variant.color }"
-              class="inline-block w-5 h-5 mr-2 capitalize transition bg-white border-8 border-white rounded-full group-hover:scale-125"
-            ></span
-            >{{ variant.color }} &nbsp;
-            <span class="text-sm italic text-gray-400"
-              >({{ variant.quantity }} items)</span
+              v-if="product.totalInvetory > stockThreshold"
+              class="px-4 py-2 text-green-700 bg-green-100 rounded"
+            >
+            <i class="fa-regular fa-circle-check mr-2 "></i> In stock
+            </span>
+
+            <span
+              v-else-if="product.totalInvetory <= stockThreshold && product.totalInvetory > 0"
+              class="px-4 py-2 text-yellow-700 bg-yellow-100 rounded"
+            >
+            <i class="fa-regular fa-triangle-exclamation mr-2 "></i> Few items remaining!
+            </span>
+
+            <span v-else class="px-4 py-2 text-red-700 bg-red-100 rounded">
+            <i class="fa-regular fa-circle-exclamation mr-2 "></i>  Sold out!
+            </span>
+          </div>
+
+          <h1 class="product-title">
+            {{ product.brand + ' ' + product.name }}
+          </h1>
+
+          <p class="product-description justify">{{ product.description }}</p>
+
+          <div class="my-4">
+            <p class="font-semibold text-2xl">Ksh. {{ product.price.toLocaleString() }}/-</p>
+          </div>
+
+          <h4>Material</h4>
+          <ul>
+            <li v-for="productDetail in product.details">
+              {{ productDetail }}
+            </li>
+          </ul>
+
+          <h4>Colors</h4>
+          <div class="flex gap-8">
+            <div
+              v-for="variant in product.variants"
+              :key="variant.id"
+              @click="updateVariant(product, variant)"
+              class="flex items-center group hover:cursor-pointer"
+            >
+              <span
+                :style="{ borderColor: variant.color }"
+                class="inline-block w-5 h-5 mr-2 capitalize transition bg-white border-8 border-white rounded-full group-hover:scale-125"
+              ></span
+              >{{ variant.color }} &nbsp;
+              <span class="text-sm italic text-gray-400"
+                >({{ variant.quantity }} items)</span
+              >
+            </div>
+          </div>
+
+          <h4>Sizes</h4>
+          <div class="flex gap-4 tracking-wide">
+            <span
+              v-for="(size, index) in product.sizes"
+              :key="index"
+              @click="updateSize(product, size)"
+              class="px-4 py-2 text-xs font-semibold bg-gray-100 border-2 border-gray-200 rounded hover:bg-white hover:cursor-pointer"
+              >{{ size.toUpperCase() }}</span
             >
           </div>
-        </div>
 
-        <h4>Sizes</h4>
-        <div class="flex gap-4 tracking-wide">
-          <span
-            v-for="(size, index) in product.sizes"
-            :key="index"
-            @click="updateSize(product, size)"
-            class="px-4 py-2 text-xs font-semibold bg-gray-100 border-2 border-gray-200 rounded hover:bg-white hover:cursor-pointer"
-            >{{ size.toUpperCase() }}</span
-          >
-        </div>
-
-        <!-- Action Button(s) -->
-        <div class="flex gap-6 mt-8 actions">
-          <button
-            :disabled="!product.selectedVariant || product.selectedVariant.quantity <= 0"
-            @click="addToCart(product.id)"
-            type="button"
-            class="px-6 py-4 text-sm font-semibold tracking-wide text-white uppercase transition bg-blue-700 rounded hover:bg-blue-600 hover:shadow-lg hover:disabled:shadow-none disabled:bg-blue-700 disabled:opacity-25 disabled:cursor-not-allowed"
-          >
-            Add to Cart
-          </button>
+          <!-- Action Button(s) -->
+          <div class="flex gap-6 mt-8 actions">
+            <button
+              :disabled="!product.selectedVariant || product.selectedVariant.quantity <= 0"
+              @click="addToCart(product.id)"
+              type="button"
+              class="px-6 py-4 text-sm font-semibold tracking-wide text-white uppercase transition bg-blue-700 rounded hover:bg-blue-600 hover:shadow-lg hover:disabled:shadow-none disabled:bg-blue-700 disabled:opacity-25 disabled:cursor-not-allowed"
+            >
+              Add to Cart
+            </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>`,
+  `,
 
   data() {
     return {
